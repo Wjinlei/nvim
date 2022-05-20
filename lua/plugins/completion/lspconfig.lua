948f5c499ffbe1b5d03a2854f4c39ae9f3a4a701
Jerry Wang <freebufer.wang@gmail.com>
2022-05-21 02:25:19 +0800

    Fixbug formating BufWritePost.

--- lua/plugins/completion/lspconfig.lua
+++ lua/plugins/completion/lspconfig.lua
@@ -84,7 +84,7 @@ local on_attach = function(client, bufnr)
 	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 	if client.supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
+		vim.api.nvim_create_autocmd("BufWritePost", {
 			group = augroup,
 			buffer = bufnr,
 			callback = function()
