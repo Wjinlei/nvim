c344d4afaade35fd10997ddf487aa2a0147c552d
Jerry Wang <freebufer.wang@gmail.com>
2022-05-21 00:18:49 +0800

    Telescope add 'project_change_files' picker.

--- lua/plugins/tools/pickers.lua
+++ lua/plugins/tools/pickers.lua
@@ -11,6 +11,13 @@ M.project_files = function()
 	end
 end
 
+M.project_change_files = function()
+	local ok = pcall(require("telescope.builtin").git_status)
+	if not ok then
+		require("telescope.builtin").find_files()
+	end
+end
+
 M.shell_error = function()
 	return vim.v.shell_error ~= 0
 end
--- lua/plugins/tools/plugins.lua
+++ lua/plugins/tools/plugins.lua
@@ -1,7 +1,8 @@
 -- luacheck: globals vim
 local tools = {}
-local keymap = vim.api.nvim_set_keymap
 local conf = require("plugins.tools.config")
+local keymap = vim.api.nvim_set_keymap
+local keymap_opts = { noremap = true, silent = true }
 
 tools["nvim-telescope/telescope.nvim"] = {
 	module = "telescope",
@@ -12,21 +13,12 @@ tools["nvim-telescope/telescope.nvim"] = {
 		{ "nvim-lua/plenary.nvim", opt = false },
 		{ "nvim-lua/popup.nvim", opt = false },
 	},
-	keymap(
-		"n",
-		"ff",
-		"<cmd>lua require('plugins.tools.pickers').project_files()<cr>",
-		{ noremap = true, silent = true }
-	),
-	keymap("n", "fs", "<cmd>lua require('plugins.tools.pickers').search()<cr>", { noremap = true, silent = true }),
-	keymap("n", "bb", "<cmd>lua require('telescope.builtin').buffers()<cr>", { noremap = true, silent = true }),
-	keymap("n", "fh", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", { noremap = true, silent = true }),
-	keymap(
-		"n",
-		"fp",
-		"<cmd>lua require'telescope'.extensions.project.project{ display_type = 'full' }<CR>",
-		{ noremap = true, silent = true }
-	),
+	keymap("n", "ff", "<cmd>lua require('plugins.tools.pickers').project_files()<cr>", keymap_opts),
+	keymap("n", "fg", "<cmd>lua require('plugins.tools.pickers').project_change_files()<cr>", keymap_opts),
+	keymap("n", "fs", "<cmd>lua require('plugins.tools.pickers').search()<cr>", keymap_opts),
+	keymap("n", "bb", "<cmd>lua require('telescope.builtin').buffers()<cr>", keymap_opts),
+	keymap("n", "fh", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", keymap_opts),
+	keymap("n", "fp", ":lua require'telescope'.extensions.project.project{ display_type = 'full' }<CR>", keymap_opts),
 }
 
 tools["nvim-telescope/telescope-frecency.nvim"] = {
@@ -39,9 +31,9 @@ tools["nvim-telescope/telescope-project.nvim"] = { after = "telescope-live-grep-
 tools["notjedi/nvim-rooter.lua"] = { after = "telescope-project.nvim", config = conf.rooter }
 
 tools["airblade/vim-gitgutter"] = {
-	keymap("n", "gn", "<Plug>(GitGutterNextHunk)", { noremap = true, silent = true }),
-	keymap("n", "gp", "<Plug>(GitGutterPrevHunk)", { noremap = true, silent = true }),
-	keymap("n", "gf", ":GitGutterFold<CR>", { noremap = true, silent = true }),
+	keymap("n", "gn", "<Plug>(GitGutterNextHunk)", keymap_opts),
+	keymap("n", "gp", "<Plug>(GitGutterPrevHunk)", keymap_opts),
+	keymap("n", "gf", ":GitGutterFold<CR>", keymap_opts),
 
 	-- Refresh gitgutter signs
 	vim.cmd([[augroup GitGutterRefresh]]),
@@ -52,7 +44,7 @@ tools["airblade/vim-gitgutter"] = {
 
 tools["dstein64/vim-startuptime"] = {
 	cmd = "StartupTime",
-	keymap("n", "<C-c>s", ":StartupTime<CR>", { noremap = true, silent = true }),
+	keymap("n", "<C-c>s", ":StartupTime<CR>", keymap_opts),
 }
 
 tools["TimUntersberger/neogit"] = {
@@ -60,26 +52,25 @@ tools["TimUntersberger/neogit"] = {
 		{ "nvim-lua/plenary.nvim", opt = false },
 	},
 	config = conf.neogit,
-	keymap("n", "gs", '<cmd>lua require("neogit").open()<CR>', { noremap = true, silent = true }),
+	keymap("n", "gs", '<cmd>lua require("neogit").open()<CR>', keymap_opts),
 }
 
 tools["aserowy/tmux.nvim"] = {
 	config = conf.tmux,
-	keymap("n", "<A-h>", '<cmd>lua require("tmux").move_left()<cr>', { noremap = true, silent = true }),
-	keymap("n", "<A-j>", '<cmd>lua require("tmux").move_bottom()<cr>', { noremap = true, silent = true }),
-	keymap("n", "<A-k>", '<cmd> lua require("tmux").move_top()<cr>', { noremap = true, silent = true }),
-	keymap("n", "<A-l>", '<cmd>lua require("tmux").move_right()<cr>', { noremap = true, silent = true }),
-
-	keymap("n", "<Left>", '<cmd>lua require("tmux").resize_left()<cr>', { noremap = true, silent = true }),
-	keymap("n", "<Down>", '<cmd>lua require("tmux").resit_bottom()<cr>', { noremap = true, silent = true }),
-	keymap("n", "<Right>", '<cmd>lua require("tmux").resize_right()<cr>', { noremap = true, silent = true }),
-	keymap("n", "<Up>", '<cmd> lua require("tmux").resize_top()<cr>', { noremap = true, silent = true }),
+	keymap("n", "<Left>", '<cmd>lua require("tmux").resize_left()<cr>', keymap_opts),
+	keymap("n", "<Down>", '<cmd>lua require("tmux").resit_bottom()<cr>', keymap_opts),
+	keymap("n", "<Right>", '<cmd>lua require("tmux").resize_right()<cr>', keymap_opts),
+	keymap("n", "<Up>", '<cmd> lua require("tmux").resize_top()<cr>', keymap_opts),
+	keymap("n", "<A-h>", '<cmd>lua require("tmux").move_left()<cr>', keymap_opts),
+	keymap("n", "<A-j>", '<cmd>lua require("tmux").move_bottom()<cr>', keymap_opts),
+	keymap("n", "<A-k>", '<cmd> lua require("tmux").move_top()<cr>', keymap_opts),
+	keymap("n", "<A-l>", '<cmd>lua require("tmux").move_right()<cr>', keymap_opts),
 }
 
 tools["akinsho/toggleterm.nvim"] = {
 	tag = "v1.*",
 	config = conf.toggleterm,
-	keymap("n", "<C-\\>", '<Cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true }),
-	keymap("i", "<C-\\>", '<Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true }),
+	keymap("n", "<C-\\>", '<Cmd>exe v:count1 . "ToggleTerm"<CR>', keymap_opts),
+	keymap("i", "<C-\\>", '<Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>', keymap_opts),
 }
 return tools
