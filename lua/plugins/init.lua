f3ea28cc8e4d193d1497b4d7d39dd6a3662d89ab
Jerry Wang <freebufer.wang@gmail.com>
2022-05-26 11:44:57 +0800

    Modify packer keyamp.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,5 +4,6 @@ local main = function()
 	local pack = require("plugins.packer")
 	pack:init()
 end
-keymap("n", "<C-x>", ":PackerSync<CR>", { noremap = true, silent = true })
+keymap("n", "<C-x>u", ":PackerSync<CR>", { noremap = true, silent = true })
+keymap("n", "<C-x>i", ":PackerInstall<CR>", { noremap = true, silent = true })
 main()
