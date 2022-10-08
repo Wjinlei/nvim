81e4d2e9cd6cdfcfa2be70f18644eeb33213f85c
Jerry Wang <freebufer.wang@gmail.com>
2022-10-08 09:56:29 +0800

    fix(config|init{.lua}): Fixbug

--- lua/plugins/completion/config.lua
+++ lua/plugins/completion/config.lua
@@ -22,7 +22,7 @@ function config.aerial()
 		--   auto    - aerial window will stay open as long as there is a visible
 		--             buffer to attach to
 		--   global  - same as 'persist', and will always show symbols for the current buffer
-		close_behavior = "auto",
+                close_automatic_events = { "unsupported" },
 
 		-- Set to false to remove the default keybindings for the aerial buffer
 		default_bindings = true,
