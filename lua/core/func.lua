ae420ffb1304bee433c859b3f19bfdc7b8546308
Jerry Wang <freebufer.wang@gmail.com>
2022-10-17 17:05:37 +0800

    chore(core): Optimize 'func.lua'.

--- lua/core/func.lua
+++ lua/core/func.lua
@@ -11,15 +11,14 @@ M.quit = function()
 end
 
 M.run = function()
-	api.nvim_command("w")
-	if vim.bo.filetype == "c" then
-		api.nvim_command("term gcc % -o %< && ./%< && rm -f %<")
-	end
 	if vim.bo.filetype == "go" then
 		api.nvim_command("term go run %<.go")
 	end
 	if vim.bo.filetype == "rust" then
 		api.nvim_command("term cargo run")
+	end
+	if vim.bo.filetype == "c" then
+		api.nvim_command("term gcc % -o %< && ./%< && rm -f %<")
 	end
 end
 
