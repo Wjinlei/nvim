b3816af952522b51783bb2247e818209dc38b84d
Jerry Wang <freebufer.wang@gmail.com>
2022-10-17 16:17:48 +0800

    fix(plugins): Fix nvim-tree.

--- lua/plugins/ui/config.lua
+++ lua/plugins/ui/config.lua
@@ -127,7 +127,6 @@ function config.nvim_tree()
 		respect_buf_cwd = false,
 		view = {
 			width = 30,
-			height = 30,
 			hide_root_folder = false,
 			side = "left",
 			preserve_window_proportions = false,
