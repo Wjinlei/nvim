3b549e54fb3dd4ab4492a15afe3854ec4a016486
Jerry Wang <freebufer.wang@gmail.com>
2022-10-21 16:04:28 +0800

    chore(core): Optimize quit keymap.

--- lua/core/func.lua
+++ lua/core/func.lua
@@ -3,22 +3,23 @@ local api = vim.api
 local M = {}
 
 M.quit = function()
-	if api.nvim_buf_get_name(api.nvim_get_current_buf()) ~= "" then
-		api.nvim_command("bdelete!")
-	else
+	local output = api.nvim_command_output("echo len(filter(range(1, bufnr('$')), 'buflisted(v:val)'))")
+	if output == "1" then
 		api.nvim_command("quit!")
+	else
+		api.nvim_buf_delete(0, { force = 1 })
 	end
 end
 
 M.run = function()
+	if vim.bo.filetype == "c" then
+		api.nvim_command("term gcc % -o %< && ./%< && rm -f %<")
+	end
 	if vim.bo.filetype == "go" then
 		api.nvim_command("term go run %<.go")
 	end
 	if vim.bo.filetype == "rust" then
 		api.nvim_command("term cargo run")
-	end
-	if vim.bo.filetype == "c" then
-		api.nvim_command("term gcc % -o %< && ./%< && rm -f %<")
 	end
 end
 
--- lua/plugins/tools/config.lua
+++ lua/plugins/tools/config.lua
@@ -13,23 +13,21 @@ function config.telescope()
 	local Job = require("plenary.job")
 	local new_maker = function(filepath, bufnr, opts)
 		filepath = vim.fn.expand(filepath)
-		Job
-			:new({
-				command = "file",
-				args = { "--mime-type", "-b", filepath },
-				on_exit = function(j)
-					local mime_type = vim.split(j:result()[1], "/")[1]
-					if mime_type == "text" then
-						previewers.buffer_previewer_maker(filepath, bufnr, opts)
-					else
-						-- maybe we want to write something to the buffer here
-						vim.schedule(function()
-							vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
-						end)
-					end
-				end,
-			})
-			:sync()
+		Job:new({
+			command = "file",
+			args = { "--mime-type", "-b", filepath },
+			on_exit = function(j)
+				local mime_type = vim.split(j:result()[1], "/")[1]
+				if mime_type == "text" then
+					previewers.buffer_previewer_maker(filepath, bufnr, opts)
+				else
+					-- maybe we want to write something to the buffer here
+					vim.schedule(function()
+						vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+					end)
+				end
+			end,
+		}):sync()
 	end
 
 	require("telescope").setup({
@@ -168,9 +166,7 @@ function config.neogit()
 			-- modify status buffer mappings
 			status = {
 				-- Adds a mapping with "B" as key that does the "BranchPopup" command
-				["B"] = "BranchPopup",
-				-- Removes the default mapping of "s"
-				["s"] = "",
+				-- ["B"] = "BranchPopup",
 			},
 		},
 	})
