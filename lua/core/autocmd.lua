0958b32a4cc917d6164bece8e6e31a6cf0cfb2dd
Jerry Wang <freebufer.wang@gmail.com>
2023-03-24 08:47:52 +0800

    fix: set eol.

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -3,8 +3,14 @@
 -- Set tabstop=4
 vim.cmd([[augroup SetTabstop]])
 vim.cmd([[autocmd! * <buffer>]])
-vim.cmd([[autocmd BufEnter <buffer> :set tabstop=8]])
-vim.cmd([[autocmd BufEnter <buffer> :set shiftwidth=8]])
-vim.cmd([[autocmd BufEnter <buffer> :set softtabstop=8]])
-vim.cmd([[autocmd BufEnter <buffer> :set expandtab]])
+vim.cmd([[autocmd BufEnter * :set tabstop=8]])
+vim.cmd([[autocmd BufEnter * :set shiftwidth=8]])
+vim.cmd([[autocmd BufEnter * :set softtabstop=8]])
+vim.cmd([[autocmd BufEnter * :set expandtab]])
+vim.cmd([[augroup END]])
+
+vim.cmd([[augroup SetEol]])
+vim.cmd([[autocmd! * <buffer>]])
+vim.cmd([[autocmd BufWrite * :set eol]])
+vim.cmd([[autocmd BufWrite * :set fixeol]])
 vim.cmd([[augroup END]])
