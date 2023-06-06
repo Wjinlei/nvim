251bf54e94420c513e785a80f9705a48c437345b
Jerry Wang <freebufer.wang@gmail.com>
2023-06-07 00:25:24 +0800

    chore: Modify tab size to 4.

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -3,9 +3,9 @@
 -- Set tabstop=4
 vim.cmd([[augroup SetTabstop]])
 vim.cmd([[autocmd! * <buffer>]])
-vim.cmd([[autocmd BufEnter * :set tabstop=8]])
-vim.cmd([[autocmd BufEnter * :set shiftwidth=8]])
-vim.cmd([[autocmd BufEnter * :set softtabstop=8]])
+vim.cmd([[autocmd BufEnter * :set tabstop=4]])
+vim.cmd([[autocmd BufEnter * :set shiftwidth=4]])
+vim.cmd([[autocmd BufEnter * :set softtabstop=4]])
 vim.cmd([[autocmd BufEnter * :set expandtab]])
 vim.cmd([[augroup END]])
 
