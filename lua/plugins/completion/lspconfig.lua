75e16bebe3211dc8add25ef44d522567637630b7
Jerry Wang <freebufer.wang@gmail.com>
2023-06-06 17:04:16 +0800

    chore: Optimize clangd cmd.

--- lua/plugins/completion/lspconfig.lua
+++ lua/plugins/completion/lspconfig.lua
@@ -23,6 +23,18 @@ lspconfig.clangd.setup({
 	capabilities = {
 		offsetEncoding = { "utf-16" },
 	},
+	cmd = {
+		"clangd",
+		"--background-index",
+		"--clang-tidy",
+		"--completion-style=detailed",
+		"--pch-storage=memory",
+		"--header-insertion=iwyu",
+		"--header-insertion-decorators",
+		"--offset-encoding=utf-16",
+		"--pretty",
+		"-j=12",
+	},
 })
 lspconfig.gopls.setup({})
 lspconfig.volar.setup({})
