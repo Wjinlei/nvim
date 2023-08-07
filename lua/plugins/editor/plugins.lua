ff8b14e0ab2ea4a5978d2507c2d377699d9b8f12
Jerry Wang <freebufer.wang@gmail.com>
2023-08-07 10:34:15 +0800

    refactor: delete nvim-ts-rainbow

--- lua/plugins/editor/plugins.lua
+++ lua/plugins/editor/plugins.lua
@@ -4,7 +4,6 @@ local keymap = vim.api.nvim_set_keymap
 local conf = require("plugins.editor.config")
 
 editor["nvim-treesitter/nvim-treesitter"] = { run = ":TSUpdate", event = "BufRead", config = conf.nvim_treesitter }
-editor["p00f/nvim-ts-rainbow"] = { after = "nvim-treesitter" }
 editor["JoosepAlviste/nvim-ts-context-commentstring"] = { after = "nvim-treesitter" }
 
 editor["windwp/nvim-spectre"] = {
