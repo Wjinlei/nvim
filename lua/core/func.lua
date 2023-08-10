45bd24c9285d1eadd81c3ea5f4127b1ec97da4ce
Jerry Wang <freebufer.wang@gmail.com>
2023-08-10 16:36:30 +0800

    refactor: delete quit function

--- lua/core/func.lua
+++ lua/core/func.lua
@@ -2,20 +2,6 @@
 local api = vim.api
 local M = {}
 
-M.quit = function()
-	-- Get buffer count
-	local cmd = "echo len(filter(range(1, bufnr('$')), 'buflisted(v:val)'))"
-	-- Execute the command and get the output
-	local cmd_output = api.nvim_command_output(cmd)
-
-	-- If it is the last buffer, exit
-	if cmd_output == "1" then
-		api.nvim_command("quit!")
-	else
-		api.nvim_buf_delete(0, { force = 1 })
-	end
-end
-
 M.run = function()
 	if vim.bo.filetype == "c" then
 		api.nvim_command("term gcc % -o %< && ./%< && rm -f %<")
