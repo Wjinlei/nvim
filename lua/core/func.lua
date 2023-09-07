230d5797c76c2adc480e2c4746ced66f4e4e65d0
Jerry Wang <freebufer.wang@gmail.com>
2023-09-07 11:42:55 +0800

    feat: optimize exit

--- lua/core/func.lua
+++ lua/core/func.lua
@@ -16,12 +16,22 @@ end
 
 -- 自定义函数，用于退出 Neovim
 M.exit = function()
+	-- 获取当前 buffer 的名字和编号
 	local current_bufname = vim.fn.bufname("%")
+	local current_bufnr = vim.fn.bufnr("%")
 
+	-- 如果当前 buffer 不是空白或者 "[No Name]"，就调用 bwipeout! 删除它
 	if current_bufname and current_bufname ~= "" and current_bufname ~= "[No Name]" then
-		vim.cmd("bwipeout! " .. vim.fn.bufnr("%"))
+		-- 添加一个短暂的延迟，确保 buffer 已经完全加载
+		vim.defer_fn(function()
+			-- 检查当前 buffer 是否仍然存在
+			if vim.api.nvim_buf_is_valid(current_bufnr) then
+				vim.cmd("bwipeout! " .. current_bufnr)
+			end
+		end, 10) -- 延迟 10 毫秒
 	else
-		vim.cmd("quit!")
+		-- 否则执行 quit
+		vim.cmd("quit")
 	end
 end
 
