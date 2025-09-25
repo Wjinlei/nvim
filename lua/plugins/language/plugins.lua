39c5933daef3f7a2bb06a962bdec94a300c42c89
Jerry Wang <freebufer.wang@gmail.com>
2025-09-25 16:38:48 +0800

    feat(language): 新增rust语言支持插件

--- lua/plugins/language/plugins.lua
+++ lua/plugins/language/plugins.lua
@@ -9,4 +9,11 @@ language["iamcco/markdown-preview.nvim"] = {
 	keymap("n", "<C-c>m", "<Plug>MarkdownPreviewToggle", { noremap = true, silent = true }),
 }
 
+-- 在 Neovim 中提升你的 Rust 体验！Rust-tools.nvim 的复刻版本
+-- https://github.com/mrcjkb/rustaceanvim  
+-- 此插件无须设置，不依赖lspconfig
+-- 请勿手动调用nvim-lspconfig.rust_analyzer 设置或设置 LSP 客户端rust-analyzer，因为这样做可能会导致冲突。
+-- 需要安装 rust_analyzer，可使用<LEADER>lsp 安装
+language["mrcjkb/rustaceanvim"] = {}
+
 return language
